ddec056c409759dbcf9a2d4c89449e0d977f46c9
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-03-04 23:54:58 +0900

    2-02 데이터를 관리하는 모델

--- django_study/pybo/apps.py
+++ django_study/pybo/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 # 이 파일에 정의된 PyboConfig 클래스가 config/settings.py 파일의 INSTALLED_APPS 항목에 추가되지 
-# 않으면 장고는 pybo 앱을 인식하지 못하고 데이터베이스 관련 작업도 할 수 없다
-# 모델은 앱에 종속되어 있으므로
+# 않으면 장고는 pybo 앱을 인식하지 못하고 데이터베이스 관련 작업도 할 수 없다.
+# 이유 -> 모델은 앱에 종속되어 있으므로
 class PyboConfig(AppConfig):
     name = 'pybo'
